7912e5ba0c123fd8e79037ec8db26ed826e32af6
Kousuke KISHI <ksh2ksk4@gmail.com>
2022-06-26 01:45:10 +0900

    Implement an echo server

--- src/clj_echo_server/core.clj
+++ src/clj_echo_server/core.clj
@@ -1,7 +1,33 @@
 (ns clj-echo-server.core
+  (:require [clojure.java.io :refer [delete-file]])
+  (:import (java.net StandardProtocolFamily UnixDomainSocketAddress)
+           (java.nio ByteBuffer)
+           (java.nio.channels ServerSocketChannel)
+           (java.nio.charset StandardCharsets))
   (:gen-class))
 
+(defonce ^:private socket-file "/tmp/.echo-server-socket")
+
+;;; https://docs.oracle.com/javase/jp/18/docs/api/java.base/java/net/UnixDomainSocketAddress.html
+;;; https://docs.oracle.com/javase/jp/18/docs/api/java.base/java/nio/channels/ServerSocketChannel.html
+;;; https://docs.oracle.com/javase/jp/18/docs/api/java.base/java/nio/channels/SocketChannel.html
+
+(defn- start-server
+  "echoサーバを起動する"
+  [socket-file]
+  (let [socket-address (UnixDomainSocketAddress/of socket-file)
+        server-socket-channel (ServerSocketChannel/open StandardProtocolFamily/UNIX)]
+    ;; ソケットファイルが残っていたら削除する
+    (delete-file socket-file true)
+    (.bind server-socket-channel socket-address)
+    (let [buffer (ByteBuffer/allocate 1024)
+          socket-channel (.accept server-socket-channel)]
+      (.read socket-channel buffer)
+      (.flip buffer)
+      (let [input (.toString (.decode StandardCharsets/UTF_8 buffer))]
+        (println "input: " input)
+        (.write socket-channel (ByteBuffer/wrap (.getBytes input StandardCharsets/UTF_8)))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (start-server socket-file))
